48654bbfefe368de0cfd4a7d844c36c98b6861b2
Omar Jallow <omjallowwatf@outlook.com>
2023-11-06 11:56:59 +0000

    Override the OnModelCreating inside the AppDbContext.

--- eMovieTicketSite/Data/AppDbContext.cs
+++ eMovieTicketSite/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eMovieTicketSite.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eMovieTicketSite.Data
 {
@@ -8,5 +9,26 @@ namespace eMovieTicketSite.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // first define the actor_movie, primary keys
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+            // define the actor_movie to be a joining table ...
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(
+                m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(
+                m => m.ActorId);
+
+            base.OnModelCreating(modelBuilder); // pass the model builder to the base class, useful in generating default authentication tables.
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
